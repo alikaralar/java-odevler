80f722474083e51adf43fa066be4b81711b4e9a2
alikaralar <85291153+alikaralar@users.noreply.github.com>
2021-06-06 13:14:20 +0300

    Update Pratik4TaksimetreProgrami.java

--- Pratik4TaksimetreProgrami.java
+++ Pratik4TaksimetreProgrami.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 public class Pratik4TaksimetreProgrami {
     public static void main(String[] args) {
-    double kmBasina = 2.20, girilenDeger, toplamTutar;
+    double kmBasina = 2.20, km, toplamTutar, acilisFiyati = 10;
     String odenecekTutar;
     System.out.print("Kaç km yol aldığınızı giriniz: ");
     Scanner input = new Scanner(System.in);
-    girilenDeger = input.nextDouble();
-    toplamTutar = girilenDeger*kmBasina;
+    km = input.nextDouble();
+    toplamTutar = (km*kmBasina)+acilisFiyati;
     odenecekTutar = (toplamTutar <= 20) ? "Toplam 20 Tl ödemeniz gerekmektedir."
             : "Toplam " + toplamTutar+ "  TL Ödemeniz gerekmektedir." ;
     System.out.println(odenecekTutar);
-
-
 
 
     }
