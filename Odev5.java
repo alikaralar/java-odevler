7dba243c98029c3d786893746a447eb2ee8b9e01
alikaralar <85291153+alikaralar@users.noreply.github.com>
2021-06-12 16:51:22 +0300

    Add files via upload

--- Odev5.java
+++ Odev5.java
@@ -1,17 +1,28 @@
+//En büyük ve en küçük sayıları bulan program.
 import java.util.Scanner;
 
 public class Odev5 {
     public static void main(String[] args) {
-        int yil;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Yıl giriniz: ");
-        yil = input.nextInt();
-        if ((yil % 4 == 0) || (yil % 400 == 0)) {
-            System.out.println(yil + " Artık Yıldır.");
+        int girdi,sayi=0,enBuyuk,enKucuk;
 
-        }else{
-            System.out.println(yil + " Artık Yıl Değildir!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz:");
+        girdi = input.nextInt();
+        enBuyuk = sayi;
+        enKucuk =sayi;
+
+        for (int i = 1; i<=girdi; i++){
+            System.out.print(i + ". Sayıyı giriniz: ");
+            sayi = input.nextInt();
+            if (sayi>enBuyuk){
+                enBuyuk=sayi;
+
+            }else if (sayi<enKucuk){
+                enKucuk=sayi;
+            }
         }
+        System.out.println("En büyük sayı: "+ enBuyuk);
+        System.out.println("En küçük sayı: "+ enKucuk);
 
     }
 }
